f716ec8dce74f3bca8229c298327243265261d0c
enhopkin@gmail.com <enhopkin@gmail.com>
2022-01-11 18:54:27 -0700

    annoying song complete

--- index.js
+++ index.js
@@ -245,8 +245,12 @@ Using the annoyingSong function below do the following:
     "{number} bottles of soda on the wall, {number} bottles of soda, take one down pass it around {number left over} bottles of soda on the wall"
 */
 
-function annoyingSong(){
+function annoyingSong(startnum){
+  for(let i=startnum; i>0; i--){
+    return `${i} bottles of soda on the wall, ${i} bottles of soda, take one down pass it around ${i-1} bottles of soda on the wall`
+  }
 }
+console.log(annoyingSong(10))
 
 
 
